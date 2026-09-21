717345c5a7fae7c47241e273fa1cabed55a360fe
José Raimundo Pereira Junior <juniorpereiraster@gmail.com>
2026-09-21 18:34:10 -0300

    exercicioDeBotao

---
+++ site-exemplo/index.js
@@ -0,0 +1,9 @@
+function botao1(){
+    return alert("Você clicou no botão 1")
+}
+function botao2(){
+    return alert("Você clicou no botão 2")
+}
+function botao3(){
+    return alert("Você clicou no botão 3")
+}
